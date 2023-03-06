d17a08c06219b20aef9df0fc0ffac2ffb116d386
LinchukLAV <84069977+LinchukLAV@users.noreply.github.com>
2023-03-06 19:30:36 +0200

    Add files via upload

---
+++ IntersectedWithPlane.hpp
@@ -0,0 +1,23 @@
+#include "Geometry\Source\Ray.hpp"
+#include "Geometry\Source\Plane.hpp"
+#include <cmath>
+
+class IntersectedWithPlane
+{
+public:
+
+    bool IntersectedWithRay(const Plane& plane, const Ray& ray)
+    {
+        Vector3d normal = plane.GetNormal();
+        Vector3d direction = ray.GetDirection();
+        Vector3d origin = ray.GetOrigin();
+        Vector3d center = plane.GetOrigin();
+
+        // ѕерев≥рка на пендендикул€рн≥сть вектор≥в
+        float dot = normal.Dot(direction);
+        if (fabs(dot) < 0.0001f) return false;
+
+        // знаходимо параметр t та визначаЇмо, де перетинаЇтьс€ пр€ма з площиною
+        return normal.Dot(origin - center) / dot >= 0;
+    }
+};
