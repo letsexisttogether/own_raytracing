b86e6b906377f1c21aa6102b75de677e3eeffd29
Alexander Chernenko <letmeexist95@gmail.com>
2023-03-08 10:42:35 +0200

    Changed Screen class - it's now uses hip instead of stack

--- RayTrace/Render/Screen.h
+++ RayTrace/Render/Screen.h
@@ -1,13 +1,49 @@
 #pragma once
 #include <iostream>
+#include <exception>
 
-template <int H, int W>
 class Screen
 {
+private:
+	char **m_Pixels;
+	uint16_t m_Height;
+	uint16_t m_Width;
+
 public:
-	char m_Screen[H][W];
+	Screen() = delete;
+	Screen(uint16_t height, uint32_t width)
+		: m_Height{ height }, m_Width{ width }
+	{
+		m_Pixels = new char*[m_Height];
+
+		for (uint16_t i = 0; i < m_Height; ++i)
+		{
+			m_Pixels[i] = new char[m_Width];
+		}
+	}
+
+	~Screen()
+	{
+		for (uint16_t i = 0; i < m_Height; ++i)
+		{
+			delete[] m_Pixels[i];
+		}
+
+		delete[] m_Pixels;
+	}
+
+	char& GetPixel(uint16_t height, uint16_t width) const noexcept(false)
+	{
+		if (height >= m_Height || m_Width >= m_Width)
+		{
+			throw std::exception("Such a pixel is out of the scree");
+		}
+
+		return m_Pixels[height][width];
+	}
+
 public:
-	void Print()
+	void Print() const noexcept
 	{
 		for (int i = H - 1; i >= 0; i--)
 		{
