acc0c97675d9c7c15bd346609826a51e2b34d18c
Alexander Chernenko <letmeexist95@gmail.com>
2023-03-08 12:09:28 +0200

    Removed pointer from Screen class

--- RayTrace/Render/Screen.h
+++ RayTrace/Render/Screen.h
@@ -1,51 +1,33 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include <exception>
 
 class Screen
 {
 private:
-	char **m_Pixels;
+	std::vector<std::vector<char>> m_Pixels;
 	uint16_t m_Height;
 	uint16_t m_Width;
 
 public:
 	Screen() = delete;
+	Screen(const Screen& screen) = default;
+
 	Screen(uint16_t height, uint16_t width)
 		: m_Height{ height }, m_Width{ width }
-	{
-		m_Pixels = new char*[m_Height];
+	{}
 
-		for (uint16_t i = 0; i < m_Height; ++i)
-		{
-			m_Pixels[i] = new char[m_Width];
-		}
-	}
-
-	~Screen()
-	{
-		for (uint16_t i = 0; i < m_Height; ++i)
-		{
-			delete[] m_Pixels[i];
-		}
-
-		delete[] m_Pixels;
-	}
+	~Screen() = default;
 
 	inline uint16_t GetHeigth() const noexcept { return m_Height; }
 	inline uint16_t GetWidth() const noexcept { return m_Width; }
 
 	char& GetPixel(uint16_t height, uint16_t width) noexcept(false)
 	{
-		if (height >= m_Height || width >= m_Width)
-		{
-			throw std::exception("Such a pixel is out of the screen");
-		}
-
-		return m_Pixels[height][width];
+		return m_Pixels.at(height).at(width);
 	}
 
-public:
 	void Print() const noexcept
 	{
 		for (int i = m_Height - 1; i >= 0; i--)
