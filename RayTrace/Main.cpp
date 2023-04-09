ff77c38df8d8091f1ba93741f9937b4a20ec7f1c
Alexander Chernenko <letmeexist95@gmail.com>
2023-04-09 10:45:11 +0300

    Synchronized main.cpp and vs files with FileProcessing

--- RayTrace/Main.cpp
+++ RayTrace/Main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-#include <array>
+#include <fstream>
+#include <string>
 
-#include "Render/Tracer.h"
-#include "Scene.h"
+#include "FileProcessing/FileReader.hpp"
 
 int main()
 {
-	float pi = 3.1415926535;
-	Camera camera = { {0.f, 0.f, 0.f}, {0.f, 0.f, 1.f}, 1.37340077 * 2.f };
-	Screen screen{ 100u, 100u, 10, camera };
+    FileReader reader{ "file.ppm" };
 
-	Vector3d light = { -10.f, -20.f, 5.f };
-	RayTracer RT{ screen, camera, light.Normalize() };
-	
-	Plane plane = { { -5.f, 0.f, 40.f }, { 10.f, 10.f, 10.f } };
+    const auto result = reader.ReadFile();
 
-	Scene scene = { RT };	
-	
-	scene.AddToScene(new Sphere({ -50.f, 50.f, 60.f }, 50.f));
-	scene.AddToScene(new Sphere({ 70.f, 0.f, 31.f }, 30.f));
-	scene.AddToScene(new Sphere({ 0.f, 0.f, 40.f }, 30.f));
-	
-	RT.Tracing(scene);
-	RT.GetScreen().Print();
+    for (const auto byte : result)
+    {
+        const char smb = static_cast<char>(byte);
+        std::cout << smb;
+    }
+
+    std::cout << "\nThe size is " << result.size() << std::endl;
+
+    return 0;
 }
