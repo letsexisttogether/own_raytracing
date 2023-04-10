76383832a24ee5090a06a6ff7774e79634157881
LinchukLAV <84069977+LinchukLAV@users.noreply.github.com>
2023-04-10 22:30:08 +0300

    Update PPMReader.hpp

--- RayTrace/FileProcessing/Readers/PPMReader.hpp
+++ RayTrace/FileProcessing/Readers/PPMReader.hpp
@@ -1,67 +1,14 @@
-#include "../PPMReader.hpp"
+#pragma once
 
-PPMReader::PPMReader(std::vector<std::byte>&& bytes)
-	: Reader<PPM>(std::move(bytes))
-{}
+#include "Reader.hpp"
+#include "../Formats/PPM.hpp"
 
-int get_int_number(int& byte_number, const std::vector<std::byte>& m_Bytes)
+class PPMReader : public Reader<PPM>
 {
-    std::string number;
+public:
+	PPMReader(std::vector<std::byte>&& bytes);
 
-    while (isdigit((char)m_Bytes[byte_number]) && (char)m_Bytes[byte_number] != ' ' && 
-        (char)m_Bytes[byte_number] != '\r' && (char)m_Bytes[byte_number + 1] != '\n')
-        number += (char)m_Bytes[byte_number++];
-   
-    byte_number++;
+	virtual ~PPMReader() override = default;
 
-    return atoi(number.c_str());
-}
-
-
-void PPMReader::Read() noexcept(false)
-{
-    {
-        int byte_number = 0;
-        int size = m_Bytes.size();
-
-        // зчитати формат PPM файлу
-        int j = 0;
-        while ((char)m_Bytes[byte_number] != '\r' && (char)m_Bytes[byte_number + 1] != '\n')
-        {
-            m_UnformattedStruct.Format[j++] = (char)m_Bytes[byte_number++];
-        }
-
-        byte_number += 2;
-
-        // зчитуємо коментарі, якщо вони там є
-        std::string comment;
-        if ((char)m_Bytes[byte_number] == '#')
-            do
-            {
-                comment.clear();
-                while ((char)m_Bytes[byte_number] != '\r' && (char)m_Bytes[byte_number + 1] != '\n')
-                    comment += (char)m_Bytes[byte_number++];
-
-                comment += "\r\n";
-
-                byte_number++;
-                m_UnformattedStruct.Comments.push_back(comment);
-
-            } while ((char)m_Bytes[++byte_number] == '#');
-
-        // зчитуємо висоту, ширину та градацію кольорів
-        m_UnformattedStruct.Width = get_int_number(byte_number, m_Bytes);
-        m_UnformattedStruct.Height = get_int_number(byte_number, m_Bytes);
-        m_UnformattedStruct.PixelMaxValue = get_int_number(byte_number, m_Bytes);
-        
-        // зчитуємо пікселі зображення
-        if ((char)m_Bytes[byte_number] == '\r') byte_number += 2;
-        else if ((char)m_Bytes[byte_number] == '\n') byte_number++;
-
-        while (size > byte_number)
-        {
-            m_UnformattedStruct.Data.push_back(get_int_number(byte_number, m_Bytes));
-        }
-    }
-}
-
+	virtual void Read() noexcept(false) override;
+};
