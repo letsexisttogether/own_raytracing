9d427bb47e15a91a7eb11ffa7b547a9640a63e2e
LinchukLAV <84069977+LinchukLAV@users.noreply.github.com>
2023-05-02 14:36:20 +0300

    Added conver method from BMP to PPM

--- RayTrace/FileProcessing/Converters/Source/PPMConverter.cpp
+++ RayTrace/FileProcessing/Converters/Source/PPMConverter.cpp
@@ -7,5 +7,21 @@ PPM PPMConverter::Convert(const PPM& ppm) const noexcept
 
 PPM PPMConverter::Convert(const BMP& bmp) const noexcept
 {
-	return PPM();
+    PPM ppm;
+
+    std::memcpy(ppm.Format, "P3", 2);
+
+    ppm.Width = bmp.Width;
+    ppm.Height = bmp.Height;
+
+    ppm.PixelMaxValue = 255;
+
+    for (std::size_t i = 0; i < bmp.Data.size(); i += 3)
+    {
+        ppm.Data.push_back(static_cast<std::uint8_t>(bmp.Data[i]));
+        ppm.Data.push_back(static_cast<std::uint8_t>(bmp.Data[i + 1]));
+        ppm.Data.push_back(static_cast<std::uint8_t>(bmp.Data[i + 2]));
+    }
+
+    return ppm;
 }
