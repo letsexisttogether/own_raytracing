e5987aebfcdc07b6366ef91bf2297e896effa77d
Alexsander Chernenko <letmeexist95@gmail.com>
2023-05-18 14:52:15 +0300

    Added inheritance of ImageFormat for BMP

--- ImageProcessors/Formats/BMP.hpp
+++ ImageProcessors/Formats/BMP.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
-// Потом переделаешь, здесь многое не нужно
-struct BMP
+#include "ImageFormat.hpp"
+
+class BMP : public ImageFormat
 {
+public:
+    void CheckHeader() const noexcept(false) override;
+
+    void AddPadding() noexcept;
+    void RemovePadding() noexcept;
+
+public:
+    struct RGBTriple
+    {
+        uint8_t Blue;
+        uint8_t Green;
+        uint8_t Red;
+    };
+
+public:
     char Header[2]; 
     std::uint32_t FileSize;
 
@@ -25,86 +41,4 @@ struct BMP
     std::uint32_t ImportantColors;
 
     std::vector<std::uint8_t> Data;
-
-    struct RGBTriple {
-        uint8_t blue;
-        uint8_t green;
-        uint8_t red;
-    };
-
-    void removePadding() {
-        int padding = (4 - (Width * sizeof(std::uint8_t) * 4) % 4) % 4;
-        int newWidth = Width * sizeof(std::uint8_t) * 4 + padding;
-        long numPixels = Data.size();
-
-        std::vector<std::uint8_t> newPixels;
-        newPixels.reserve(numPixels);
-
-        int rowIndex = 0;
-        for (int y = Height - 1; y >= 0; y--)
-        {
-            int tempY = y * Width * 4;
-            int tempWidth = newWidth + y * Width * 4;
-            for (long i = tempY; i < tempWidth; i += 4) {
-                newPixels.push_back(Data[i + 2]);
-                newPixels.push_back(Data[i + 1]);
-                newPixels.push_back(Data[i]);
-                rowIndex++;
-
-                if (rowIndex == Width) {
-                    // Досягнуто кінця рядка, пропускаємо вирівнювальні байти
-                    i += padding;
-                    rowIndex = 0;
-                }
-            }
-        }        
-
-        Data = std::move(newPixels);
-    }
-
-    void addPadding() {
-        int padding = (4 - Width % 4) % 4;
-        int newWidth = Width * 4 + padding;
-        long numPixels = Data.size();
-        int numRows = Height;
-
-        int newNumPixels = numRows * newWidth;
-
-        std::vector<std::uint8_t> newPixels;
-        newPixels.reserve(newNumPixels);
-
-        for (int y = Height - 1; y >= 0; y--)
-        {
-            int tempY = y * Width * 3;
-            int tempWidth = Width * 3 + y * Width * 3;
-            for (long i = tempY; i < tempWidth; i += 3)
-            {
-                /*newPixels.push_back(Data[originalIndex++]);
-                newPixels.push_back(Data[originalIndex++]);
-                newPixels.push_back(Data[originalIndex++]);*/
-                
-                newPixels.push_back(Data[i + 2]);                
-                newPixels.push_back(Data[i + 1]);                
-                newPixels.push_back(Data[i]);
-                newPixels.push_back(static_cast <std::uint8_t>(0));
-                
-            }
-
-            /*for (int i = 0; i < padding; ++i) {
-                newPixels.push_back(static_cast <std::uint8_t>(0));
-                newPixels.push_back(static_cast <std::uint8_t>(0));
-                newPixels.push_back(static_cast <std::uint8_t>(0));
-                newPixels.push_back(static_cast <std::uint8_t>(0));
-            }*/
-        }
-
-        /*for (long i = 0; i < numPixels; i += 3) {
-            newPixels.push_back(Data[i + 2]);
-            newPixels.push_back(Data[i + 1]);
-            newPixels.push_back(Data[i]);          
-            newPixels.push_back(static_cast <std::uint8_t>(0));            
-        }*/
-
-        Data = std::move(newPixels);
-    }
 };
---
+++ ImageProcessors/Formats/Source/BMP.cpp
@@ -0,0 +1,67 @@
+#include "../BMP.hpp"
+
+void BMP::CheckHeader() const noexcept(false)
+{
+
+}
+
+void BMP::RemovePadding() noexcept
+{
+    int padding = (4 - (Width * sizeof(std::uint8_t) * 4) % 4) % 4;
+    int newWidth = Width * sizeof(std::uint8_t) * 4 + padding;
+    long numPixels = Data.size();
+
+    std::vector<std::uint8_t> newPixels;
+    newPixels.reserve(numPixels);
+
+    int rowIndex = 0;
+    for (int y = Height - 1; y >= 0; y--)
+    {
+        int tempY = y * Width * 4;
+        int tempWidth = newWidth + y * Width * 4;
+        for (long i = tempY; i < tempWidth; i += 4)
+        {
+            newPixels.push_back(Data[i + 2]);
+            newPixels.push_back(Data[i + 1]);
+            newPixels.push_back(Data[i]);
+            rowIndex++;
+
+            if (rowIndex == Width)
+            {
+                i += padding;
+                rowIndex = 0;
+            }
+        }
+    }
+
+    Data = std::move(newPixels);
+}
+
+void BMP::AddPadding() noexcept
+{
+    int padding = (4 - Width % 4) % 4;
+    int newWidth = Width * 4 + padding;
+    long numPixels = Data.size();
+    int numRows = Height;
+
+    int newNumPixels = numRows * newWidth;
+
+    std::vector<std::uint8_t> newPixels;
+    newPixels.reserve(newNumPixels);
+
+    for (int y = Height - 1; y >= 0; y--)
+    {
+        int tempY = y * Width * 3;
+        int tempWidth = Width * 3 + y * Width * 3;
+        for (long i = tempY; i < tempWidth; i += 3)
+        {
+            newPixels.push_back(Data[i + 2]);
+            newPixels.push_back(Data[i + 1]);
+            newPixels.push_back(Data[i]);
+            newPixels.push_back(static_cast <std::uint8_t>(0));
+
+        }
+    }
+
+    Data = std::move(newPixels);
+}
