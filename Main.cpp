d747b8b5fd30d770c3d9f7715c2d7e49981cd7fc
Alexsander Chernenko <letmeexist95@gmail.com>
2023-05-30 16:01:26 +0300

    Changed main to test ObjReader

--- Main.cpp
+++ Main.cpp
@@ -12,50 +12,28 @@
 #include "Graphics/RenderHandler/ImageRenderHandler.hpp" 
 #include "Graphics/Render/Scene.h"
 #include "Graphics/Render/Tracer.h"
+#include "ObjReader/ObjReader.hpp"
 
 
 std::int32_t main(std::uint32_t argc, const char* argv[])
 {
-	Vector3d light{ 0.f, 1.f, 0.f };
-	light = light.Normalize();
-
-	Camera camera{ { 0.f, 0.f, 0.f }, { 0.f, 0.f, 1.f }, 3.1415 / 6.f };
-	Screen screen{ 400, 560u, 10.f, camera };
-
-	FabricSelector selector{ "ImageProcessors" };
-	selector.FindDlls();
-
-	WriterFabric& writerFabric = selector.GetWriterFabric("bmp");
-	writerFabric.LoadDll();
-
-	RayTracer RT
+	try
 	{
-		new ImageRenderHandler
-		{
-			screen,
-			writerFabric.GetWriter("some_file.bmp")
-		},
-		camera, 
-		light
-	};
+		FileReader reader{ "Test.obj" };
+		
+		ObjReader objReader{ reader.ReadFile() };
+		auto m_Intersectables = objReader.Read();
 
-	Scene scene{ RT };
-	
-	scene.AddToScene(new Sphere(Vector3d{ 0.f, 0.f, 0.7 }, 0.15f));
-	
-	RT.Trace(scene);
+		std::cout << "Success";
+		
+	}
+	catch (const std::exception& exp)
+	{
+		std::cout << exp.what() << std::endl;
 
-	Matrix matrix;
+		return EXIT_FAILURE;
+	}
 
-	Vector3d vec = { 12.f, 15.f, 20.f };
-
-	matrix.AddMoving({ 1.f, 1.f, 1.f });
-
-	matrix.AddScale({ 2.f, 1.f, 1.f });
-
-	vec = matrix.GetTransformedVector(vec);
-	
-	RT.GetRenderHandler().ExecuteRenderResult();
 
 	return EXIT_SUCCESS;
 }
